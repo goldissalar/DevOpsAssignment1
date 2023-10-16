96dee1817777c714d04fc3d8cc9d8e586974c19b
goldis <goldis.salarmaaf@hotmail.com>
2023-10-16 20:23:29 +0200

    sdfsdfsdf

---
+++ web-app/web-app/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    root: true,
+    parser: 'babel-eslint', // or another parser
+    extends: ['eslint:recommended'], // Use recommended ESLint rules
+    rules: {
+      // Add specific rules here, if needed
+    },
+  };
+  
