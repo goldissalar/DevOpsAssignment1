b6503afad86dbf98a85ba674aac1c725b1a39cb8
goldis <goldis.salarmaaf@hotmail.com>
2023-10-17 19:52:46 +0200

    kjhjuh

--- web-app/web-app/.eslintrc.js
+++ web-app/web-app/.eslintrc.js
@@ -1,11 +1,3 @@
 module.exports = {
-  root: true,
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false, // If you don't have a separate Babel configuration file
-  },
-  extends: ['eslint:recommended'],
-  rules: {
-    // Add specific rules here, if needed
-  },
+  parser: "@babel/eslint-parser",
 };
